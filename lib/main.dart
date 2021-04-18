027e4856a3c3dd33bdff23fe5344115a9d94ac08
danoco78 <danoco78@gmail.com>
2021-04-18 08:35:01 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() async {
 
 class MyApp extends StatefulWidget {
   // This widget is the root of your application.
-
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -31,8 +30,7 @@ class _MyAppState extends State<MyApp> {
     nombreController.text = userPreference.nombre;
     apellidoController.text = userPreference.apellido;
     edadController.text = userPreference.edad.toString();
-
-    miMusica = userPreference.favoriteMusic;
+    miMusica = userPreference.musicaFavorita;
 
     super.initState();
   }
@@ -41,6 +39,7 @@ class _MyAppState extends State<MyApp> {
     nombreController.text = userPreference.nombre;
     apellidoController.text = userPreference.apellido;
     edadController.text = userPreference.edad.toString();
+    miMusica = userPreference.musicaFavorita;
   }
 
   @override
@@ -50,74 +49,77 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("Almacenamiento local"),
         ),
-        body: Container(
-          padding: EdgeInsets.all(8),
-          child: Column(
-            children: [
-              TextField(
-                controller: nombreController,
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(4), hintText: "Nombre"),
-                onChanged: (String valueNombre) {
-                  print(valueNombre);
-                  userPreference.nombre = valueNombre;
-                },
-              ),
-              TextField(
-                controller: apellidoController,
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(4), hintText: "Apellido"),
-                onChanged: (String valueApellido) {
-                  print(valueApellido);
-                  userPreference.apellido = valueApellido;
-                },
-              ),
-              TextField(
-                controller: edadController,
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(4), hintText: "Edad"),
-                onChanged: (String valueEdad) {
-                  print(valueEdad);
-                  try {
-                    userPreference.edad = int.parse(valueEdad);
-                  } catch (e) {}
-                },
-              ),
-              DropdownButton(
-                  value: userPreference.estadoCivil, //Valor por defecto
-                  isExpanded: true,
-                  items: [
-                    DropdownMenuItem(
-                      child: Text("Casado"),
-                      value: true,
-                    ),
-                    DropdownMenuItem(
-                      child: Text("Soltero"),
-                      value: false,
-                    ),
-                  ],
-                  onChanged: (bool valueCasado) {
-                    setState(() {
-                      userPreference.estadoCivil = valueCasado;
-                    });
-                  }),
-              musicCheckbox(),
-              Spacer(),
-              ElevatedButton.icon(
-                  //onPressed: userPreference.clean,
-                  onPressed: () {
-                    setState(() {
-                      userPreference.clean();
-                      //setState(() {});
-                      init();
-                    });
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              children: [
+                TextField(
+                  controller: nombreController,
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(4), hintText: "Nombre"),
+                  onChanged: (String valueNombre) {
+                    print(valueNombre);
+                    userPreference.nombre = valueNombre;
                   },
-                  icon: const Icon(Icons.delete),
-                  label: Text("Limpiar"),
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.red))),
-            ],
+                ),
+                TextField(
+                  controller: apellidoController,
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(4), hintText: "Apellido"),
+                  onChanged: (String valueApellido) {
+                    print(valueApellido);
+                    userPreference.apellido = valueApellido;
+                  },
+                ),
+                TextField(
+                  controller: edadController,
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(4), hintText: "Edad"),
+                  onChanged: (String valueEdad) {
+                    print(valueEdad);
+                    try {
+                      userPreference.edad = int.parse(valueEdad);
+                    } catch (e) {}
+                  },
+                ),
+                DropdownButton(
+                    value: userPreference.estadoCivil, //Valor por defecto
+                    isExpanded: true,
+                    items: [
+                      DropdownMenuItem(
+                        child: Text("Casado"),
+                        value: true,
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Soltero"),
+                        value: false,
+                      ),
+                    ],
+                    onChanged: (bool valueCasado) {
+                      setState(() {
+                        userPreference.estadoCivil = valueCasado;
+                        FocusScope.of(context).requestFocus(FocusNode());
+                      });
+                    }),
+                musicCheckbox(),
+                //Spacer(),
+                ElevatedButton.icon(
+                    //onPressed: userPreference.clean,
+                    onPressed: () {
+                      setState(() {
+                        userPreference.clean();
+                        //setState(() {});
+                        init();
+                      });
+                    },
+                    icon: const Icon(Icons.delete),
+                    label: Text("Limpiar"),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.red))),
+              ],
+            ),
           ),
         ),
       ),
@@ -125,16 +127,28 @@ class _MyAppState extends State<MyApp> {
   }
 
   Widget musicCheckbox() {
-    final music = ['Rock', 'Pop', 'Salsa', 'Vallenato'];
+    List<String> musica = ['Rock', 'Pop', 'Salsa', 'Vallenato'];
 
-    return Row(
-      children: music
+    return Column(
+      children: musica
           .map(
-            (m) => Checkbox(
-                value: true,
+            (m) => CheckboxListTile(
+                title: Text(m),
+                secondary: Icon(Icons.music_note),
+                activeColor: Colors.orange[900],
+                // checkColor: Colors.black,
+                value: miMusica.indexOf(m) >= 0,
                 onChanged: (_) {
-                  miMusica.add(m);
+                  if (miMusica.indexOf(m) < 0) {
+                    miMusica.add(m);
+                  } else {
+                    miMusica.remove(m);
+                  }
                   print(miMusica);
+                  userPreference.musicaFavorita = miMusica;
+
+                  setState(() {});
+                  FocusScope.of(context).requestFocus(FocusNode());
                 }),
           )
           .toList(),
